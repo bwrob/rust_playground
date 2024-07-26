762b3407e824f75a00cde471c97728cd22fef4a6
bwrob <bw2002@wp.pl>
2024-07-26 13:28:36 +0200

    unwinnable game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, Ferris!");
-}
+    println!("Hello, Ferris here!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+
+    let mut secret_number = rand::thread_rng().gen_range(1..=3);
+    let guess :i32 = guess
+        .trim().parse()
+        .expect("Please type a number!");
+    secret_number += guess;
+
+
+    println!("The secret number is: {secret_number}");
+    println!("You guessed: {guess}");
+    println!("You lost!");
+} 

c6b74aa4a7179f3112f141bc61451b60c0d0411a
bwrob <bw2002@wp.pl>
2024-07-27 23:43:15 +0200

    converge to chapter implementation

--- src/chapters/chapter_2.rs
+++ src/chapters/chapter_2.rs
@@ -1,26 +1,35 @@
 use std::io;
+
 use rand::Rng;
+use std::cmp::Ordering;
 
 pub fn guessing_game() {
-    println!("Hello, Ferris here!");
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}");
+        let mut guess = String::new();
 
-    let mut secret_number = rand::thread_rng().gen_range(1..=3);
-    let guess :i32 = guess
-        .trim().parse()
-        .expect("Please type a number!");
-    secret_number += guess;
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("The secret number is: {secret_number}");
-    println!("You guessed: {guess}");
-    println!("You lost!");
-} 
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use crate::chapters::chapter_2::guessing_game;
 pub mod chapters;
 
 fn main() {
+    println!("Hello, Ferris here!");
+    
     guessing_game();
 }
